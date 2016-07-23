48d58de9301897bf9207e30648bbed343fb9d3a3
agulado <9948686@qq.com>
2016-07-23 17:38:11 +0800

    1. 在dist文件夹中，增加package.json 2. 将dist发布到npm：TopuNet-GrayScale

--- dist/jq/GrayScale.js
+++ dist/jq/GrayScale.js
@@ -1,12 +1,15 @@
 /*
-    v1.1.1
+    v1.1.2
     高京
     20160324
-    图片变灰滤镜配套js
+    将图片变为灰色，可选鼠标悬停后是否复原
 */
 
 var GrayScale = {
     init: function(para) {
+
+        this.includeCSS("/inc/GrayScale.css");
+
         var para_default = {
             box_selector: null, // 作用范围选择器，如：body.index。无默认值
             hover_restore: true // 鼠标悬停时是否还原图片（即取消灰色滤镜），默认true
@@ -27,6 +30,15 @@ var GrayScale = {
                 $(this).removeAttr("style");
             });
         }
+    },
+
+    includeCSS: function(path) {
+        var a = document.createElement("link");
+        a.type = "text/css";
+        a.rel = "stylesheet";
+        a.href = path;
+        var head = document.getElementsByTagName("head")[0];
+        head.appendChild(a);
     }
 };
 
